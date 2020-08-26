3e3f96588ef5600ae83a8065fd5844e34080d033
Diego Cabrera Nieto <a01633860@itesm.mx>
2020-08-25 20:23:11 -0500

    Solve word-reverser
    Signed-off-by: Diego Cabrera Nieto <a01633860@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    // Place your magic here
+#define MAX_SIZE 1000
+
+int main()
+{
+    char c;
+    char word[MAX_SIZE];
+    int count = 0;
+
+    while ((c = getchar()) != EOF) {
+        if(c == '\n'){
+            for (int i = count; i >= 0; i--){
+                printf("%c", word[i]);
+            }
+            printf("\n");
+            count = 0;
+        }
+        else{
+            word[count] = c;
+            count++;
+        }
+    }
+    printf("\n");
     return 0;
 }
