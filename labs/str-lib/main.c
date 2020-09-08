c2e3173e63e84955df47f1602875dc554e0c23b9
Diego Cabrera Nieto <a01633860@itesm.mx>
2020-09-08 09:37:59 -0500

    s

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *str);
+int mystrfind(char *origin, char *substr);
+char *mystradd(char *origin, char *addition);
+
+int main(int argc, char* argv[]) {
+    if (strncmp(argv[1], "-add", 4) == 0){
+        printf("ADD");
+        int initialLength = mystrlen(argv[2]);
+        printf("Initial Length: %d\n", initialLength);
+        char *newStr = mystradd(argv[2], argv[3]);
+        printf("New String: %s\n", newStr);
+        int newLentgth = mystrlen(newStr);
+        printf("New Length: %d\n", newLentgth);
+
+        free(newStr);
+    }
+    else if(strncmp(argv[1], "-find", 4) == 0){
+        int wasFound = mystrfind(argv[2], argv[3]);
+        if (wasFound >= 0){
+            printf("['%s'] string was found at [%d] position\n", argv[3], wasFound);
+        }
+        else{
+            printf("Substring not found");
+        }
+    }
+    else{
+        printf("There was an error with the arguments, please try again\n");
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,45 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int c = 0;
+    while (str[c] != '\0'){
+        c++;
+    }
+    return c;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int originLength = mystrlen(origin);
+    int additionLength = mystrlen(addition);
+    char *newWord = (char *)malloc(originLength+additionLength);
+    for (int i = 0; i < originLength; i++){
+        newWord[i] = origin[i];
+    }
+    for (int j = 0; j < additionLength; j++){
+        newWord[originLength+j] = addition[j];
+    }
+    return newWord;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int substrLength = mystrlen(substr);
+    int originLength = mystrlen(origin);
+
+    if (substrLength > originLength){
+        return -1;
+    }
+    
+    int found = -1;
+
+    for(int i = 0; i < originLength; i++){
+        if (origin[i] == substr[0]){
+            for(int j = 0; j < substrLength; j++){
+                if (origin[i+j] == substr[j]){
+                    found = i;
+                }
+            }
+        }
+    }
+
+    return found;
 }
