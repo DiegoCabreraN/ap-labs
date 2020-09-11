7343fd3a88296fdaf3353972d91d5507daf44057
Diego Cabrera Nieto <a01633860@itesm.mx>
2020-09-11 13:25:11 -0500

    Solve first-partial
    Signed-off-by: Diego Cabrera Nieto <a01633860@itesm.mx>

--- challenges/first-partial/pacman-analyzer.c
+++ challenges/first-partial/pacman-analyzer.c
@@ -7,7 +7,6 @@
 #include <fcntl.h>
 #include <string.h>
 
-#define REPORT_FILE "packages_report.txt"
 #define MAX_PACKAGES 1000000
 
 struct row {
@@ -18,30 +17,31 @@ struct row {
     int updatesCounter;
 } database[MAX_PACKAGES];
 
-void analizeLog(char *logFile, char *report);
-char *GetPackageName(char contentLine[], int startCharPos, int bufferSize);
-char *GetSubstring(char contentLine[], int startCharPos, int endCharPos);
-void SaveDataToRow(char *name, int event, char* date);
-void PrintGeneralData();
-void PrintIndividualData();
+void analizeLog(char *inFile, char *report);
+char *getPackageName(char contentLine[], int startCharPos, int bufferSize);
+char *getSubstring(char contentLine[], int startCharPos, int endCharPos);
+void saveDataToRow(char *name, int event, char* date);
+void writeGeneralData(int file);
+void writeIndividualData(int file);
+void writeToFile(char *destinationFile);
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("Usage:./pacman-analizer.o \n");
+    if (argc < 5 || strcmp(argv[1],"-input") != 0 || strcmp(argv[3],"-report") != 0) {
+        printf("Usage:./pacman-analizer.o -input <inputFile> -report <destinationFile>\n");
         return 1;
     }
 
-    analizeLog(argv[1], REPORT_FILE);
+    analizeLog(argv[2], argv[4]);
 
     return 0;
 }
 
-void analizeLog(char *logFile, char *report) {
-    printf("Generating Report from: [%s] log file\n", logFile);
+void analizeLog(char *inFile, char *report) {
+    printf("Generating Report from: [%s] log file\n", inFile);
 
     // Implement your solution here.
     int fileStream;
-    int file = open(logFile, O_RDONLY);
+    int file = open(inFile, O_RDONLY);
     char buffer;
 
     char *stateName[3] = {"installe", "removed ", "upgraded"};
@@ -56,14 +56,14 @@ void analizeLog(char *logFile, char *report) {
 
     while ((fileStream = read(file, &buffer, 1)) > 0){
         if (buffer == '\n'){
-            char *actualState = GetSubstring(contentLine, 26, 34);
+            char *actualState = getSubstring(contentLine, 26, 34);
             for(int i = 0; i < 3; i++){
                 if(strcmp(actualState, stateName[i])==0){
                     int start = i == 0 ? 36 : i == 1? 34 : 35;
-                    char *packageName = GetPackageName(contentLine, start, BUFSIZ);
-                    char *eventDate = GetSubstring(contentLine, 1, 17);
+                    char *packageName = getPackageName(contentLine, start, BUFSIZ);
+                    char *eventDate = getSubstring(contentLine, 1, 17);
                     
-                    SaveDataToRow(packageName, i, eventDate);
+                    saveDataToRow(packageName, i, eventDate);
                     
                     free(eventDate);
                     free(packageName);
@@ -79,14 +79,13 @@ void analizeLog(char *logFile, char *report) {
         charPos++;
     }
     // Print Results
-    PrintGeneralData();
-    PrintIndividualData();
+    writeToFile(report);
 
     close(file);
     printf("Report is generated at: [%s]\n", report);
 }
 
-char *GetPackageName(char contentLine [], int startCharPos, int bufferSize){
+char *getPackageName(char contentLine [], int startCharPos, int bufferSize){
     char *name = malloc(sizeof(char)*100);
     int nextChar = 0;
     for(int i = startCharPos; i<bufferSize; i++){
@@ -101,7 +100,7 @@ char *GetPackageName(char contentLine [], int startCharPos, int bufferSize){
     return name;
 }
 
-char *GetSubstring(char contentLine [], int startCharPos, int endCharPos){
+char *getSubstring(char contentLine [], int startCharPos, int endCharPos){
     char *substring = malloc(sizeof(char)*(endCharPos-startCharPos+1));
     for (int i = 0; i<endCharPos-startCharPos; i++){
         substring[i] = contentLine[i+startCharPos];
@@ -110,7 +109,7 @@ char *GetSubstring(char contentLine [], int startCharPos, int endCharPos){
     return substring;
 }
 
-void SaveDataToRow(char *name, int event, char *date){
+void saveDataToRow(char *name, int event, char *date){
     int successFlag = 0;
     for (int i = 0; i < MAX_PACKAGES; i++){
         if(strcmp(database[i].packageName, "") == 0){
@@ -147,7 +146,7 @@ void SaveDataToRow(char *name, int event, char *date){
     }
 }
 
-void PrintGeneralData(){
+void writeGeneralData(int file){
     int updated = 0;
     int removed = 0;
     for (int i = 0; i < MAX_PACKAGES; i++){
@@ -160,28 +159,59 @@ void PrintGeneralData(){
             }
         }
         else{
-            printf("\nPacman Packages Report\n");
-            printf("----------------------\n");
-            printf("- Installed packages : %d\n", i);
-            printf("- Removed packages   : %d\n", removed);
-            printf("- Upgraded packages  : %d\n", updated);
-            printf("- Current installed  : %d\n\n", i-removed);
+            char* word = (char*)malloc(sizeof(char)*25);
+            
+            sprintf(word, "Pacman Packages Report\n");
+            write(file, word, strlen(word));
+            sprintf(word, "----------------------\n");
+            write(file, word, strlen(word));
+            sprintf(word, "- Installed packages : %d\n",i);
+            write(file, word, strlen(word));
+            sprintf(word, "- Removed packages   : %d\n", removed);
+            write(file, word, strlen(word));
+            sprintf(word, "- Upgraded packages  : %d\n", updated);
+            write(file, word, strlen(word));
+            sprintf(word, "- Current installed  : %d\n",i-removed);
+            write(file, word, strlen(word));
+
+            free(word);
             break;
         }
     }
-    
 }
 
-void PrintIndividualData(){
-    printf("\nList of packages\n");
-    printf("----------------\n");
+void writeIndividualData(int file){
+    char* word = (char*)malloc(sizeof(char)*25);
+    sprintf(word,"\nList of packages\n");
+    write(file, word, strlen(word));
+    sprintf(word,"----------------\n");
+    write(file, word, strlen(word));
     for (int i = 0; i < MAX_PACKAGES; i++){
         if(strcmp(database[i].packageName, "") != 0){
-            printf("- Package Name        : %s\n",database[i].packageName);
-            printf("  - Install date      : %s\n",database[i].installDate);
-            printf("  - Last update date  : %s\n",database[i].lastUpdate);
-            printf("  - How many updates  : %d\n",database[i].updatesCounter);
-            printf("  - Removal date      : %s\n",database[i].removalDate);
+            sprintf(word,"- Package Name        : %s\n",database[i].packageName);
+            write(file, word, strlen(word));
+            sprintf(word,"  - Install date      : %s\n",database[i].installDate);
+            write(file, word, strlen(word));
+            sprintf(word,"  - Last update date  : %s\n",database[i].lastUpdate);
+            write(file, word, strlen(word));
+            sprintf(word,"  - How many updates  : %d\n",database[i].updatesCounter);
+            write(file, word, strlen(word));
+            sprintf(word,"  - Removal date      : %s\n",database[i].removalDate);
+            write(file, word, strlen(word));
         }
     }
+    free(word);
+}
+
+void writeToFile(char *destinationFile){
+    int fileStream;
+    int file = open(destinationFile, O_RDWR | O_CREAT | O_TRUNC, 0644);
+    if (file == -1){
+        printf("Error while creating destination file\n");
+        exit(EXIT_FAILURE);
+    }
+    writeGeneralData(file);
+    writeIndividualData(file);
+
+    close(file);
 }
