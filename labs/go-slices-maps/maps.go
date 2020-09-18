56ba55e41f20a8a6b7f841663cf28070295bbe82
Diego Cabrera Nieto <a01633860@itesm.mx>
2020-09-18 16:17:04 -0500

    Solved go-slices-maps
    Signed-off-by: Diego Cabrera Nieto <a01633860@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	occurences := make(map[string]int)
+	splited := strings.Fields(s)
+
+	for _, val := range splited {
+		occurences[val] += 1
+	}
+	return occurences
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -3,7 +3,17 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+	p := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		p[i] = make([]uint8, dx)
+	}
+
+	for y := 0; y < dy; y++ {
+		for x := 0; x < dx; x++ {
+			p[y][x] = uint8(x ^ y*x/2)
+		}
+	}
+	return p
 }
 
 func main() {
