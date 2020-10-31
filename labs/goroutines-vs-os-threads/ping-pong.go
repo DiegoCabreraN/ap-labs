822a64511fc31ba3cb4127a1e82a14d21cbe3fec
DiegoCabreraN <diegocabreranieto@gmail.com>
2020-10-31 15:15:53 -0600

    Solved goroutines-vs-os-threads
    Signed-off-by: DiegoCabreraN <diegocabreranieto@gmail.com>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,67 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
+
+var (
+	startTime     time.Time
+	comunications int
+	message1      chan int
+	message2      chan int
+)
+
+func pong(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+
+		if id == 1 {
+			<-message2
+			message1 <- 1
+			comunications++
+		} else {
+			<-message1
+			message2 <- 2
+			comunications++
+		}
+
+		transcurredTime := time.Now().Sub(startTime).Seconds()
+
+		if transcurredTime > 1.0 {
+			if id == 1 {
+				<-message2
+				comunications++
+			} else {
+				<-message1
+				comunications++
+			}
+			break
+		}
+
+	}
+	wg.Done()
+
+}
 
 func main() {
 	var commsPerSecond int
+	var wg sync.WaitGroup
 
+	startTime = time.Now()
+
+	message1 = make(chan int)
+	message2 = make(chan int)
+
+	wg.Add(2)
+	go pong(1, &wg)
+	go pong(2, &wg)
+
+	message1 <- 1
+
+	wg.Wait()
+
+	commsPerSecond = comunications
 	fmt.Println("Communications Per Second : ", commsPerSecond)
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,51 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"time"
 )
 
-func main() {
-	var maxStages, transitTime int
+var (
+	stages int64
+)
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+func pipeTest() (int, int64) {
+	var executionTime int
+	genericChannel := make(chan int64)
+	startTime := time.Now()
+
+	defer func(startTime time.Time) {
+		if goroutine := recover(); goroutine != nil {
+			fmt.Println("Maximum number of pipeline stages   : ", stages)
+			startTime := time.Now()
+			for ; stages > 0; stages-- {
+				genericChannel <- stages
+			}
+			executionTime = int(time.Now().Sub(startTime).Seconds())
+			fmt.Println("Time to transit trough the pipeline : ", executionTime)
+		}
+	}(startTime)
+
+	for {
+		go func() {
+			stages++
+			<-genericChannel
+		}()
+		printMemUsage()
+	}
+
+}
+
+func printMemUsage() { //Limit Process Memory Usage
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	if m.TotalAlloc/1024/1024 > 1024 {
+		panic(fmt.Sprintf("MAX MEMORY REACHED"))
+	}
+}
+
+func main() {
+	runtime.GOMAXPROCS(1)
+	pipeTest()
+	runtime.GC()
 }
